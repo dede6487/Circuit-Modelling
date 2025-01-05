d14690d6ca3ffc80f25af5b6eac00dfca2be3724
Felix_Desktop <dede6487@gmail.com>
2025-01-05 21:27:31 +0100

    continued coding BDFk method

--- Matlab/BDFk.m
+++ Matlab/BDFk.m
@@ -1,8 +1,8 @@
-function y = BDFk(M, A, f, y0, tspan, h,k)
-% BDF-k method for solving My' = Ay + f
-    % M: Matrix
-    % A: matrix
-    % f: function f(t)
+function y = BDFk(A, B, f, y0, tspan, h,k)
+% BDF-k method for solving Ay' + By = f or Ay' = f - By or Ay' + By - f = 0
+    % A: Matrix
+    % B: Matrix
+    % f: vector-valued function f(t)
     % y0: Initial condition - vector of size k
     % tspan: Time interval [t0, tf]
     % h: Step size
@@ -22,8 +22,8 @@ function y = BDFk(M, A, f, y0, tspan, h,k)
     BDF2alpha = [3 -4 1];
     BDF3alpha = [11 -18 9 -2];
 
-    BDFalpha =  {BDF1alpha,
-                BDF2alpha,
+    BDFalpha =  {BDF1alpha;
+                BDF2alpha;
                 BDF3alpha};
 
     BDFbeta = [1 2 6 12 60 60];
@@ -32,8 +32,8 @@ function y = BDFk(M, A, f, y0, tspan, h,k)
     for i = k+1:n
         ti = t(i);
         % Solve implicit equation for y_i
-        F = @(yi) M*yi - h*BDFbeta(k)*(A*yi + f(ti)) - sum(y(:, i-1:i-k)*BDFalpha{k},2);
-        y(:, i) = fsolve(F, y(:, i-1));  % Use fsolve to solve the implicit equation
+        y(:, i) = (-sum(y(:, i-1:i-k)*BDFalpha{k},2) + BDFbeta{k}*h*f(ti))/(BDFalpha{k}(k)*M+BDFbeta{k}*h*B);  %solve the implicit equation
+        %not sure if this summation makes sense
     end
 end
 
